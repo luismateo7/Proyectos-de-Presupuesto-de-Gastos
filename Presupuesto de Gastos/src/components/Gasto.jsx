fa57a050af8a9b4840495574e71331fa705983fc
Mateo Rodriguez <luismateor73@gmail.com>
2022-12-22 23:33:49 -0500

    Component Gastos para mostrar cada Gasto

---
+++ Presupuesto de Gastos/src/components/Gasto.jsx
@@ -0,0 +1,10 @@
+
+const Gasto = () => {
+  return (
+    <div>
+        <p>Desde Gasto.jsx</p>
+    </div>
+  )
+}
+
+export default Gasto
--- Presupuesto de Gastos/src/components/ListadoGastos.jsx
+++ Presupuesto de Gastos/src/components/ListadoGastos.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import Gasto from "./Gasto"
 
 const ListadoGastos = ({gastos}) => {
   return (
     <div className='listado-gastos contenedor'>
         <h2>{gastos.length > 0 ? 'Gastos:' : 'No hay gastos aún'}</h2>
 
-        
+        {gastos.map(gasto => (
+          <Gasto
+            gasto={gasto}
+            key={gasto.id}
+          />
+        ))}
     </div>
   )
 }
